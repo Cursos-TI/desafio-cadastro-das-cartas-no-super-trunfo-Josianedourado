46a9aaac9be42d62f8d60018f5fcf20bcee3f52f
Josianedourado <josiane.dourado62@gmail.com>
2025-03-29 18:56:27 -0300

    Atualizar o CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -14,13 +14,13 @@ opção:P01 opção:P01
 
 nome1: Belém  nome2: castanhal
 
-população1: 1.303.403  população2:192.256
+população1: = 1.303.403  população2: =192.256
 
-turismo1:30    turismo2:
+turismo1: =30    turismo2: =10
 
-area1:1.059,47     área2:1.029,300
+area1: =1.059,47     área2: =1.029,300
 
-pib1: 33,4      pib1:4,29
+pib1: =33,4      pib1: =4,29
 
 
 
