ec1449e99b00a3b5c30be2410743a43c6213f616
Josianedourado <josiane.dourado62@gmail.com>
2025-03-29 20:24:40 -0300

    Atualizar o CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,25 +2,27 @@
 
 int main(){
 
-char nome;
-char opção;
+char nome[20];
+char nome[20]
+char letra;
 int população;
 int turismo;
-float área;
+double área;
 float pib;
 
-opção:P01 opção:P01
+nome = Para
 
+letra1 ='A1';  letra1 = 'A2';
 
-nome1: Belém  nome2: castanhal
+nome1= Belém;   nome2 = castanhal;
 
-população1: = 1.303.403  população2: =192.256
+população1 = 1.303.403;  população2 =192.256;
 
-turismo1: =30    turismo2: =10
+turismo1 =30;   turismo2 =10;
 
-area1: =1.059,47     área2: =1.029,300
+area1 =1.059,47;    área2 =1.029,300;
 
-pib1: =33,4      pib1: =4,29
+pib1 = 33,4;     pib1 = 4,29;
 
 
 
