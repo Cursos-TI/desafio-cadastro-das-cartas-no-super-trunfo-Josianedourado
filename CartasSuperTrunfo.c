d583b7dc9c37bcc99db365831b5712e5e0488204
Josianedourado <josiane.dourado62@gmail.com>
2025-03-29 18:38:28 -0300

    Atualizar o CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,32 @@
 #include <stdio.h>
 
-int main()
+int main(){
+
+char nome;
+char opção;
+int população;
+int turismo;
+float área;
+float pib;
+
+opção:P01 opção:P01
+
+
+nome1: Belém  nome2: castanhal
+
+população1: 1.303.403  população2:192.256
+
+turismo:     turismo:
+
+area1:      área2:
+
+pib1:       pib1:
+
+
+
+
+
+
+
 return 0;
+}
