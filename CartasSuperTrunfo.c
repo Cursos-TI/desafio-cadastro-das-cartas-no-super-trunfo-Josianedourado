5b56177dc636659ffbbd29cae69ec0171eab7764
Josianedourado <josiane.dourado62@gmail.com>
2025-03-31 10:12:47 -0300

    Update CartasSuperTrunfo.c

---
+++ CartasSuperTrunfo.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){            
+
+char Estado[20] = "Para";
+char carta1[] = "A01";
+char carta2 []= "A02";
+char cidade1[20] = "Belem";
+char  cidade2[20] = "Castanhal";
+int populacao1 = 1303403;
+int populacao2 = 192256;
+int turismo1 = 30;
+int turismo2 = 10;
+float Area1 = 1059.47;
+float Area2 = 1029.30;  
+float Pib1 = 33.4;
+float Pib2 = 4.29;
+
+printf("Desafio super trunfo inciante\n\n");
+
+
+printf("estado: %s\n",Estado);
+printf("carta1: %s\n", carta1);
+printf("cidade1: %s\n", cidade1);
+printf("populacao1: %d\n", populacao1);
+printf("turismo1: %d\n", turismo1);
+printf("Area1:%.2f\n", Area1);
+printf("pib1: %.2f\n\n", Pib1);
+
+
+printf("Desafio super trunfo iniciante\n\n");
+
+printf("Estado: %s\n", Estado);
+printf("Carta2: %s\n", carta2);
+printf("Cidade2: %s\n", cidade2);
+printf("população2: %d\n", populacao2);
+printf("Turismo2: %d\n", turismo2);
+printf("Area2: %.2f\n", Area2);
+printf("Pib2: %.2f\n\n", Pib2);
+
+
+return 0;
+}
